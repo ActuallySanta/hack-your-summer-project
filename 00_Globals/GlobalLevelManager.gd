2ac19c96dd0acc53075cec7d22329b6c8cedf8cc
Zamijah <zamijah.shakeurtomp@bison.howard.edu>
2026-06-18 01:32:21 -0400

    Implement Player and Tilemap Objects
    Created Player object along with scripts and spritesheets, implemented player movement with draft scripts (need to be updated to fit gameplay), started creating tilemap

---
+++ 00_Globals/GlobalLevelManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var curr_tilemap_bounds : Array[ Vector2 ]
+signal TileMapBoundsChanged( bounds : Array[ Vector2 ] )
+
+func ChangeTilemapBounds( bounds : Array[ Vector2 ] ) -> void:
+	curr_tilemap_bounds = bounds
+	TileMapBoundsChanged.emit( bounds )
