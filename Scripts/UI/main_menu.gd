1a3ace9f7a084ce91e6b484b9ae744d1e1d35829
Santiago Benitez-Fernandez <SantiBenitezFer@outlook.com>
2026-06-19 20:11:36 -0600

    Began scaffolding out the Main Menu

---
+++ Scripts/UI/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _start_game() -> void:
+	pass
+
+func _load_game() -> void:
+	pass
+
+func _open_credits_menu() -> void:
+	pass
+
+func _quit_game() -> void:
+	pass
