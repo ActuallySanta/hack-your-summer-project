fc4c6c75b458d6574e149e862d9e518da96645dc
Zamijah <zamijah.shakeurtomp@bison.howard.edu>
2026-06-26 01:00:24 -0400

    Implemented Player animations and animationtree
    Currently run, jum, and idle are implemented, the rest have just been created from the spritesheet

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -1,3 +1,3 @@
-extends Node
-
-var player : Player
+#extends Node
+#
+#var player : Player
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,16 @@
-class_name Player extends CharacterBody2D
+extends CharacterBody2D
+## Get animationtree ##
+@onready var anim_tree: AnimationTree = $AnimationTree
+@onready var playback = anim_tree.get("parameters/playback") # Controls the transitions
 
-var cardinal_direction : Vector2 = Vector2.DOWN
-var direction : Vector2 = Vector2.ZERO
-
-@onready var animation_player : AnimationPlayer = $AnimationPlayer
-@onready var state_machine : PlayerStateMachine = $StateMachine
-
-@export var moveSpeed := 500.0
-@export var jumpForce := 600.0
+@export var moveSpeed := 250.0
+@export var jumpForce := 300.0
 @export var jumpBufferTime := 0.25
 @export var coyoteTime := 0.2
-
-signal DirectionChanged( new_direction : Vector2 )
 
 var _moveInput : float
 var _jumpBufferTimer : float
 var _coyoteTimer : float
-
-func _ready() -> void:
-	PlayerManager.player = self
-	state_machine.Initialize(self)
 
 func jump() -> void:
 	velocity.y = -jumpForce
@@ -49,6 +40,23 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+	# --- ANIMATION TREE ENGINE LINK ---
+	# 1. Check if we are mid-air (jumping or falling)
+	if not is_on_floor():
+		playback.travel("jump")
+	else:
+		# 2. If we are grounded, swap between running and idling
+		if velocity.x != 0:
+			playback.travel("run")
+		else:
+			playback.travel("idle")
+			
+	# 3. Flip the sprite visually based on which way we are running
+	if _moveInput > 0:
+		$Character.flip_h = false  # Facing Right
+	elif _moveInput < 0:
+		$Character.flip_h = true   # Facing Left
+
 func set_jump_input() -> void:
 	_jumpBufferTimer = jumpBufferTime
 
@@ -57,20 +65,6 @@ func handle_inputs() -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		set_jump_input()
 
-func _process(delta: float) -> void:
+func _process( _delta: float) -> void:
 	handle_inputs()
 	#animation code would go here eventually
-
-func UpdateAnimation( state : String ) -> void:
-	#if state == "RESET":
-		#animation_player.play("RESET")  # just play RESET with no direction suffix
-	#else:
-	animation_player.play(state + "_" + AnimDirection())
-
-func AnimDirection() -> String:
-	if cardinal_direction == Vector2.DOWN:
-		return "down"
-	elif cardinal_direction == Vector2.UP:
-		return "up"
-	else:
-		return "side"
