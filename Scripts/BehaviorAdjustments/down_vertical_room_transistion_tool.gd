3ecbf7e12b6390acdb3744f2c77c08adbc17c375
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2026-07-10 18:25:52 -0400

    Fixed bug with updated colliders and vertical doors

--- Scripts/BehaviorAdjustments/down_vertical_room_transistion_tool.gd
+++ Scripts/BehaviorAdjustments/down_vertical_room_transistion_tool.gd
@@ -7,19 +7,16 @@ extends Node2D
 
 var _player_started_through_here = false
 var _apply_helpful_force = false
+var _door_closed = false
 var _player_node : CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	doorLockCollider.set_deferred("disabled", true)
-	await  get_tree().physics_frame
-	await  get_tree().physics_frame
-	await  get_tree().physics_frame
-	var overlapping_bodies = InitQuad.get_overlapping_bodies()
-	
-	for i in overlapping_bodies:
-		if i.is_in_group("player"):
-			_player_started_through_here = true
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	_player_started_through_here = verify_player_location( InitQuad )
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -31,6 +28,14 @@ func _process(delta: float) -> void:
 	
 	_player_node.velocity.y -= 40
 
+func verify_player_location(quad: Area2D) -> bool:
+	var overlapping_bodies = quad.get_overlapping_bodies()
+	
+	for i in overlapping_bodies:
+		if i.is_in_group("player"):
+			return true
+	return false
+
 func _on_initial_quadrant_body_entered(body: Node2D) -> void:
 	await get_tree().physics_frame
 	if _player_started_through_here:
@@ -41,16 +46,23 @@ func _on_initial_quadrant_body_entered(body: Node2D) -> void:
 		_player_node = body
 
 func _on_initial_quadrant_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	await get_tree().physics_frame
+	if verify_player_location( InitQuad ):
+		return
+	if body.is_in_group("player") and not _door_closed:
 		door.play("DoorOpen")
 		_apply_helpful_force = false
-		_player_started_through_here = false
+		_door_closed = true
 		doorLockCollider.set_deferred("disabled", false)
 
 
 func _on_balance_quadrant_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	await get_tree().physics_frame
+	if verify_player_location( BalanceQuad ):
+		return
+	if body.is_in_group("player") and _door_closed:
 		doorLockCollider.set_deferred("disabled", true)
 		var curr_frame = door.frame
 		door.play("DoorClose")
+		_door_closed = false
 		door.frame = 5-curr_frame
--- Scripts/Player/player_collision_manager.gd
+++ Scripts/Player/player_collision_manager.gd
@@ -11,7 +11,6 @@ var colliders
 var states
 var _active_collision
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_active_collision = State.WALK
 	colliders = {
@@ -25,7 +24,6 @@ func swap_active_collision(name: State):
 	set_disabled(name, false)
 	_active_collision = name
 
-# If this causes a crash or error because of the dictionary; switch to match statement
 func set_disabled(name: State, disabled: bool):
 	var collider = colliders[name]
 	collider.set_deferred("disabled", disabled)
