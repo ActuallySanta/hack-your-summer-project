f203c03e78f8461f6aacb5618f11b82fad6e9675
Santiago Benitez-Fernandez <SantiBenitezFer@outlook.com>
2026-07-12 11:18:18 -0600

    Got rid of demo project and added game win trigger

--- Scripts/UI/game_over_menu.gd
+++ Scripts/UI/game_over_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+#Return to main menu
 func _on_main_menu_button_2_pressed() -> void:
 	SceneLoader.LoadScene("uid://dbr2sfm8y3rni")
 
@@ -8,3 +8,8 @@ func _on_main_menu_button_2_pressed() -> void:
 func _quit_game() -> void:
 	get_tree().quit()
 	pass
+
+#Reload from last checkpoint
+func _on_main_menu_button_pressed() -> void:
+	SceneLoader.LoadScene("uid://5uhi1nn3ykxt")
+	pass # Replace with function body.
