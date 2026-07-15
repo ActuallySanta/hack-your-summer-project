b04e7835a762f7348e386fef2303fba32e9e3c3c
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2026-07-14 23:02:54 -0400

    Added some sfx

--- ai/tasks/get_hurt.gd
+++ ai/tasks/get_hurt.gd
@@ -2,6 +2,10 @@ extends BTAction
 
 func _enter() -> void:
 	agent.velocity = Vector2.ZERO
+	var kids = agent.get_children()
+	for kid in kids:
+		if kid.name == "HitSFX":
+			kid.play()
 
 func _tick(delta: float) -> Status:
 	await agent.get_tree().create_timer(agent.stunDuration).timeout
