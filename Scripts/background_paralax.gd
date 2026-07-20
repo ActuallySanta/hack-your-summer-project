fa2c7aa3b241bbac48e251cb0ffedd7d4b11ed6a
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2026-07-19 23:30:09 -0400

    Implemented DockingBay Zoo

--- Scripts/background_paralax.gd
+++ Scripts/background_paralax.gd
@@ -1,27 +1,32 @@
-extends TileMapLayer
+extends Node2D
 
 @export var factor = 0.2
 
-var _camera
-var _init_pos : float
-var _is_active : bool
+var _camera : Camera2D
+var _init_pos : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_is_active = false
 	await get_tree().physics_frame
-	_is_active = true
 	_camera = get_viewport().get_camera_2d()
-	_init_pos = get_camera_pos_y()
-	global_position.y = _init_pos
+	_init_pos = get_camera_pos()
+	global_position = _init_pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not _is_active:
+	if is_no_camera():
 		return
-	var current_height = get_camera_pos_y()
-	var _distance_traveled = current_height - _init_pos
-	global_position.y = _init_pos + _distance_traveled * factor
+	var current_pos = get_camera_pos()
+	var _distance_traveled = current_pos - _init_pos
+	global_position = _init_pos + _distance_traveled * factor
 
-func get_camera_pos_y() -> float:
-	return _camera.global_position.y
+func is_no_camera() -> bool:
+	if _camera == null:
+		_camera = get_viewport().get_camera_2d()
+		return _camera == null
+	return false
+
+func get_camera_pos() -> Vector2:
+	if is_no_camera():
+		return Vector2.ZERO
+	return _camera.global_position
