10ed89c01fa605f334c516937ef86c43efb8dfef
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2026-07-22 18:53:16 -0400

    Fixed bug in Tileset physics

--- Scripts/Resources/Tools/Fallen_Cyborg_Sprite_Connector_For_Debugging.gd
+++ Scripts/Resources/Tools/Fallen_Cyborg_Sprite_Connector_For_Debugging.gd
@@ -7,5 +7,3 @@ func _notification(what: int) -> void:
 		if parent and "start_flipped" in parent:
 			# Set flip_h to the value of the parent's variable
 			flip_h = parent.start_flipped
-	else:
-		set_process(false)
