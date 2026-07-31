96a84fbc931c09a91ba4758613498b553f608f7d
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2026-07-30 21:57:34 -0700

    Remove unused code

--- Scripts/EnvironmentalHazards/liquid_of_doom_and_death.gd
+++ Scripts/EnvironmentalHazards/liquid_of_doom_and_death.gd
@@ -2,11 +2,8 @@ extends TileMapLayer
 
 @onready var areaCollider : CollisionShape2D = $Area2D/CollisionShape2D
 
-var is_player_in_doom : bool
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	is_player_in_doom = false
 	var used_rect := get_used_rect()
 	var cell_size := tile_set.tile_size
  
@@ -21,19 +18,3 @@ func _ready() -> void:
 	var map_center: Vector2 = pixel_pos + (pixel_size / 2.0)
 	$Area2D.global_position = to_global(map_center)
 	areaCollider.set_deferred("disabled", false)
-
-func _process(delta: float) -> void:
-	if not is_player_in_doom:
-		return
-	
-
-func _on_body_entered(body: Node2D) -> void:
-	if not body.is_in_group("player"):
-		return
-	is_player_in_doom = true
-
-
-func _on_body_exited(body: Node2D) -> void:
-	if not body.is_in_group("player"):
-		return
-	is_player_in_doom = false
