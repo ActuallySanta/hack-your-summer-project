baba6a27023f02504e176c882eeb6bde09c450d7
Santiago Benitez-Fernandez <SantiBenitezFer@outlook.com>
2026-08-02 00:22:13 -0600

    Added extra enemy attacks

--- ai/tasks/spawn_eye.gd
+++ ai/tasks/spawn_eye.gd
@@ -2,8 +2,8 @@ extends BTAction
 
 enum AttackType{
 	basicAttack,
-	shotgunAttack,
-	cornerAttack
+	cornerAttack,
+	randomAttack
 }
 
 @export var currAttackType : AttackType = AttackType.basicAttack
@@ -23,6 +23,34 @@ func _tick(delta: float) -> Status:
 			agent.add_child(instance)
 			instance.reparent(agent.get_tree().current_scene)
 			instance.linear_velocity = instance.global_position.direction_to(PlayerManager.player.global_position)*projectileSpeed
+			return Status.SUCCESS
+		AttackType.randomAttack:
+			for i in 4:
+				var instance : BossBullet = eyeProjectile.instantiate()
+				var randPos : Vector2 = Vector2(
+					randf_range(user.eyeSpawnArea.position.x,user.eyeSpawnArea.size.x),
+					randf_range(user.eyeSpawnArea.position.y,user.eyeSpawnArea.size.y))
+				instance.position = randPos
+				agent.add_child(instance)
+				instance.reparent(agent.get_tree().current_scene)
+				instance.linear_velocity = instance.global_position.direction_to(PlayerManager.player.global_position)*projectileSpeed
+			return Status.SUCCESS
+		AttackType.cornerAttack:
+			var TRCornerInstance : BossBullet = eyeProjectile.instantiate()
+			var TLCornerInstance : BossBullet = eyeProjectile.instantiate()
+			var BRCornerInstance : BossBullet = eyeProjectile.instantiate()
+			var BLCornerInstance : BossBullet = eyeProjectile.instantiate()
+			
+			TLCornerInstance.global_position = Vector2(user.eyeSpawnArea.position.x,user.eyeSpawnArea.position.y)
+			TRCornerInstance.global_position = Vector2(user.eyeSpawnArea.size.x,user.eyeSpawnArea.position.y)
+			BLCornerInstance.global_position = Vector2(user.eyeSpawnArea.position.x,user.eyeSpawnArea.size.y)
+			BRCornerInstance.global_position = Vector2(user.eyeSpawnArea.size.x,user.eyeSpawnArea.size.y)
+			
+			var instances : Array[BossBullet] = [TLCornerInstance,BRCornerInstance,BLCornerInstance,TRCornerInstance]
+			
+			for i in instances.size():
+				instances[i].reparent(agent.get_tree().current_scene)
+				instances[i].linear_velocity = instances[i].global_position.direction_to(user.eyeSpawnArea.get_center())*projectileSpeed
 			return Status.SUCCESS
 		_:
 			print("No Attack Type Selected")
