acc580cd84574d31df499ff79cf4ac5ef72dc3ef
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2026-09-13 02:27:13 -0400

    Did some work on player interactable

--- Scripts/EnvironmentalHazards/player_interactable.gd
+++ Scripts/EnvironmentalHazards/player_interactable.gd
@@ -1,5 +1,6 @@
 class_name PlayerInteractable extends Area2D
 
+@onready var image_controller := $"Image controller"
 
 var _player_in_interactable
 
@@ -8,11 +9,15 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not _player_in_interactable:
+		return
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		_player_in_interactable = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_player_in_interactable = false
