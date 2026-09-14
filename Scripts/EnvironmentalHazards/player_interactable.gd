d74249f591b32454d44a088778fa31ddf496ce8e
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2026-09-13 22:32:37 -0400

    Finished raw code for player_interactable

--- Scripts/EnvironmentalHazards/player_interactable.gd
+++ Scripts/EnvironmentalHazards/player_interactable.gd
@@ -1,23 +1,58 @@
 class_name PlayerInteractable extends Area2D
 
-@onready var image_controller := $"Image controller"
+signal on_player_confirm_interaction
+signal on_player_start_interaction
+signal on_player_fail_interaction 
 
-var _player_in_interactable
+@export var interact_once : bool
+@export var velocity_cutoff_point : float = 0
+@export var time_to_succeed : float = 1.0
+
+@onready var node_to_scale := $ImageScaler
+
+var _player_reference : Player
+var _player_in_interactable : bool
+var _successfully_interacted : bool
+var _timer : float 
+var _init_y_scale : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	_init_y_scale = node_to_scale.scale.y
 
 func _process(delta: float) -> void:
-	if not _player_in_interactable:
+	# If we can't interact with this node in the future, we shouldn't waste time doing logic for it
+	if not _can_interact():
 		return
+	if _player_reference.velocity.length() <= velocity_cutoff_point and _can_interact() and _timer >= 1:
+		confirm_interaction()
+		return
+	
+	# Increment the timer/weight and update accordingly
+	var increment = delta / time_to_succeed
+	if not _player_in_interactable:
+		increment *= -1
+	_timer = clamp(_timer + increment, 0, 1)
+	update_visuals()
 
+func confirm_interaction() -> void:
+	on_player_confirm_interaction.emit()
+	_successfully_interacted = true
+
+func update_visuals() -> void:
+	node_to_scale.y = lerp(_init_y_scale, 0, _timer)
+
+func _can_interact() -> bool:
+	return not interact_once or not _successfully_interacted
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and _can_interact():
 		_player_in_interactable = true
+		on_player_start_interaction.emit()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		_player_reference = body as Player
 		_player_in_interactable = false
+		if not _successfully_interacted:
+			on_player_fail_interaction.emit()
